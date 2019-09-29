b159314c86992183ab50cfc957010cb75975d030
Khaïs Colin <kh.col@orange.fr>
2019-09-29 18:08:23 +0200

    initial commit

---
+++ simple_example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+win = Gtk.Window()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
+
